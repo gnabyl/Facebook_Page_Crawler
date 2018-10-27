886eecca2fc422b8583c3136826d69e3c523eed0
Nguyen Le Ly Bang <bangjdev@gmail.com>
2018-10-27 14:14:22 +0700

    Update: now can work with many urls

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-	
-	static String username;
-	static String password;
+	static String username, password, inputFile;
 	static Scanner sc = new Scanner(System.in);
 	
 	public static void main(String args[]) {
@@ -13,13 +16,30 @@ public class Main {
 		password = sc.nextLine();
 		
 		String outputFolder = ".";
-		if (args.length != 0)
-			outputFolder  = args[0];
+		inputFile = "./fanpage_list.txt";
+		if (args.length != 0) {
+			inputFile = args[0];
+			outputFolder  = args[1];
+		}
 		
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(new File(inputFile)));
+			String line = null;
+			while ((line = br.readLine()) != null) {
+				if (line.lastIndexOf('/') == line.length() - 1)
+					line = line.substring(0, line.length() - 1);
+				FBCrawler crawler = new FBCrawler("https://www.facebook.com", line, username, password, outputFolder);
+				crawler.startCrawler();
+				crawler.doCrawling();
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		FBCrawler crawler_tuyetcollection = new FBCrawler("https://www.facebook.com", "https://www.facebook.com/We.Are.vOzer", username, password, outputFolder);
-		crawler_tuyetcollection.startCrawler();
-		crawler_tuyetcollection.doCrawling();
 		
 	}
 }
